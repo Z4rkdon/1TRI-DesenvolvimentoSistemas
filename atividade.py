24d3c0dc2b7784cf36ba49764c2c88acc9db342e
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-02-20 08:51:25 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,24 @@
+print("Olá Mundo!")
+nome = "Guilherme"
+idade = 16
+altura = 1.66
+genero = "Masculino"
+cor_de_pele = "Branco"
+cor_de_cabelo = "Ruivo"
+tamanho_cabelo = "Grande"
+estilo_cabelo = "Nenhum"
+#usar o print com variáveis 
+print("Olá, o meu nome é {nome}, minha idade é {idade}, minha altura é {altura}, meu genero {genero}, minha cor de pele {cor_de_pele}, cor do meu cabelo {cor_de_cabelo}, tamanho do cabelo{tamanho_cabelo}, meu estilo de cabelo é{estilo_cabelo} ")
+
+print("Curso")
+nome_curso = "Desenvolvimento de sistemas"
+série = 3
+#usar o print com variáveis 
+print("Olá eu sou do curso {nome_curso}, minha série é {série}")
+
+print("Hobby")
+favorito = "Escutar musicas"
+gosto_mas_nao_faço_muito = "Desenhar"
+favorito_também = "ler e jogar"
+#usar o print com variáveis 
+print("Olá, meu hobby favorito é {favorito}, não faço muito {gosto_mas_nao_faço_muito}, meu favorito também {favorito_também}")
