3d167e0e64095db893e7ab4e7e4403018f916eae
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-03-05 12:30:46 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,29 @@
+# menu de opições usado no terminal
+while True:
+    print("1 - Opção 1")
+    print("2 - Opção 2")
+    print("3 - Opção 3")
+    print("4 - Opção 4")
+    print("5 - sair")
+
+    opcao = input("Digite a opção desejada (1 a 5)")
+
+    if opcao == '1':
+        print("Selecionado opção 1")
+    elif opcao == '2':
+        print("Selecionado opção 2")
+    elif opcao == '3':
+        print("Selecionado opção 3")
+    elif opcao == '4':
+        print("Selecionado opção 4")
+    elif opcao == '5':
+        print("Selecionado sair")
+        break
+    else:
+        print("Opção Incorreta")
+
+    
+    
+    
+
+   
