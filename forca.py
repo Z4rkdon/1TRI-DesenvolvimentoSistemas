6b70aedd82ddf8eb18bb7689ebc8fec36dfc1646
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-03-23 09:44:05 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,15 +1,24 @@
-palavra = "HILDA"
-letras_acertadas = ["_", "_", "_", "_", "_"]
+import random
+palavras = ["HILDA", "ESCOLA", "GATO", "JAVASCRIPT", "COMBO"]
+palavra = random.choice(palavras)
+
+letras_acertadas = []
+for letra in palavra:
+    letras_acertadas.append("_")
+
 acertou = False
+enforcou = False
+limite_tentativas = len(palavra) + 6
+tentativa = 1
 
 def mostrar_letras_acertadas():
     for letra in letras_acertadas:
         print(letra, end=" ")
 
-print("Tente adivinhar a palavra secreta: ") 
-while(not acertou):
+print("Tente adivinhar a palavra secreta: ")
+while(not acertou and not enforcou):
+    # mostrar as letras acertadas
     mostrar_letras_acertadas()
-    
     print("")
     chute = input("Digite uma letra: ")
     indice = 0
@@ -17,12 +26,25 @@ while(not acertou):
         if chute.upper() == letra:
             letras_acertadas[indice] = letra
         indice = indice + 1
+    
+    if tentativa == limite_tentativas:
+        print("Você perdeu :(\nA palavra era: ", palavra)
+        enforcou = True
 
     if letras_acertadas.count("_") == 0:
         print("Parabéns, você acertou a palavra secreta!")
         mostrar_letras_acertadas()
         acertou = True
 
+    tentativa = tentativa + 1
 
 
-        
+# TABELA VERDADE
+# Quero fazer um suco de banana OU maça
+# banana = true
+# maçã = true
+#   E       |       OU
+# v v = v   |     v v = v
+# V F = F   |     V F = V
+# F V = F   |     F V = V
+# F F = F   |     F F = F
