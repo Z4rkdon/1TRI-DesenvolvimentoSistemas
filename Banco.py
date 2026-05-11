8dcc8bdf8f435e10faaf8ddc6335c2fc81d66ceb
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-05-11 09:43:48 -0300

    Update Banco.py

--- Banco.py
+++ Banco.py
@@ -1,4 +1,4 @@
-class  Banco:
+class  conta:
     def __init__(self, titular, agencia, numero):
         self.__titular = titular
         self.__agencia = agencia
@@ -9,4 +9,17 @@ class  Banco:
         print(f"O saldo da {self.__titular} é {self.__saldo}")    
 
     def deposito(self, valor):
-         self.__saldo = self.__saldo + valor
+        if valor > 0:
+           self.__saldo = self.__saldo + valor
+           print("Depósito efetuado com sucesso!")
+        else:
+            print("Não foi póssivel efetuar o depósito")
+
+    def saque(self, valor):
+        if valor <= self.__saldo and valor > 0:
+            self.__saldo = self.__saldo - valor
+            print("Saque efetuado com sucesso!")
+        else:
+            print("Erro em efetuar o saque")
+ 
+    def transferir(self, conta_destino, valor):
