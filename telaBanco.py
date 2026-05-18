a305ce86bbe6c3b19f1a03856ec76f22a2241fab
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-05-18 09:45:18 -0300

    Update telaBanco.py

--- telaBanco.py
+++ telaBanco.py
@@ -1,8 +1,14 @@
-from conta import conta
+from conta import Conta
 conta1 = conta("Guilherme", 1643, "1759-7")
 conta1.extrato()
 conta1.deposito(1000)
 conta1.saque(200)
 conta1.saque(1500)
 conta1.extrato()
-conta1.transferir(Conta2, 300)
+conta2 = Conta("Guilherme", 1234, "7070-0")
+conta2.extrato()
+conta1.transferir(300, conta2)
+conta1.extrato()
+conta2.extrato()
+conta2.titular = "Guilherme"
+print(f"Olá, {conta2.titular}! :)")
