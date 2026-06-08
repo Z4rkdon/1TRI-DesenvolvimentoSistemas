5e20cb5dde63ddbd72148627cc20bb100d6f7abb
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-06-08 09:46:17 -0300

    Update telaGerente.py

--- telaGerente.py
+++ telaGerente.py
@@ -5,8 +5,10 @@ import json
 def login():
     if input_titular.get() == "admin":
         if input_agencia.get() == "1234":
-            if input_cpf.get(): == "6789":
+            if input_cpf.get() == "6789":
                 label_resposta.configure(text="Login realizado com sucesso!!", fg="green")
+            else:
+                label_resposta.configure(text="Falha no login", fg="red")
         else:
             label_resposta.configure(text="Falha no login", fg="red")
     else:
@@ -14,47 +16,58 @@ def login():
 
 def cadastrar():
     conta = Conta(input_titular.get(), input_agencia.get(), input_cpf.get())
-    with open("clientes.json", "r") as clientes_arq
-      clientes = json.load(clientes_arq)
+    print(conta.extrato())
+    print(conta.numero)
 
-    clientes_append({
-        "titular": conta.titular
-        "agencia": conta.agencia
-        "numero": conta.numero
-        "cpf": conta.cpf
-        "saldo": conta.saldo
-        "senha:" conta.senha
+    with open("clientes.json", "r") as clientes_arq:
+        clientes = json.load(clientes_arq)
+
+    clientes.append({
+        "titular": conta.titular,
+        "agencia": conta.agencia,
+        "numero": conta.numero,
+        "cpf": conta.cpf,
+        "saldo": conta.saldo,
+        "senha": conta.senha,
         "chavepix": conta.chavepix
     })
+    
     with open("clientes.json", "w") as clientes_escritas:
         json.dump(clientes, clientes_escritas, indent=4)
+        
     label_resposta.configure(
-        text=f"Conta: {conta.numero} Titulat {conta.titular} cadastrado com sucesso!!!",
+        text=f"Conta: {conta.numero} Titular {conta.titular} cadastrado com sucesso!!!",
         fg="green")
 
 app = tk.Tk()
 app.title("Banco Red Dessert")
-app.geometry("400x300")
+app.geometry("400x400") 
 
 label_titular = tk.Label(app, text="titular:")
 label_titular.pack(pady=5)
 input_titular = tk.Entry(app)
 input_titular.pack()
 
-label_agência = tk.Label(app, text="agência:")
-label_agência.pack(pady=5)
-input_agência = tk.Entry(app)
-input_agência.pack()
+
+label_agencia = tk.Label(app, text="agência:")
+label_agencia.pack(pady=5)
+input_agencia = tk.Entry(app)
+input_agencia.pack()
 
 label_cpf = tk.Label(app, text="cpf:")
 label_cpf.pack(pady=5)
 input_cpf = tk.Entry(app)
 input_cpf.pack()
 
-botao = tk.Button(app, text="Enviar", command=cadastrar)
-botao.pack(pady=5)
+
+botao_cadastrar = tk.Button(app, text="Cadastrar", command=cadastrar)
+botao_cadastrar.pack(pady=5)
+
+botao_login = tk.Button(app, text="Login", command=login)
+botao_login.pack(pady=5)
 
 label_resposta = tk.Label(app, text="")
-label1_senha.pack(pady=5)
+
+label_resposta.pack(pady=5)
 
 app.mainloop()
